3844c4f368c41f14e26be03c1abad25d56397750
Wayne <wabbruscatoorr@gmail.com>
2020-09-15 13:27:03 -0400

    Updating RestExceptionHandler, ErrorDetail-Model, CoursesServicesImpl and StudentServiceImpl

--- schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -1,7 +1,41 @@
 package com.lambdaschool.schools.handlers;
 
+import com.lambdaschool.schools.exceptions.ResourceNotFoundException;
+import com.lambdaschool.schools.models.ErrorDetail;
+import com.lambdaschool.schools.services.HelperFunctions;
+import org.hibernate.id.uuid.Helper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler
 {
+    @Autowired
+    HelperFunctions helperFunctions;
+    public RestExceptionHandler()
+    {
+        super();
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe)
+    {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setDetails(rnfe.getMessage());
+        errorDetail.setDevelopermessage(rnfe.getClass().getName());
+        errorDetail.setErrors(helperFunctions.getConstraintViolations(rnfe));
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
 }
